22ba6d27c5b227af1b8171a8be70f213c1227441
wenithqp-cloud <wenithqp@gmail.com>
2026-01-24 23:18:07 -0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,4 @@
-
+/* SIDE MENU HOVER */
+const sideMenu = document.getElementById("sideMenu");
+sideMenu.addEventListener("mouseenter", () => sideMenu.classList.add("expanded"));
+sideMenu.addEventListener("mouseleave", () => sideMenu.classList.remove("expanded"));
